d026f08d8ad0ea1d16dd40ef28eee123b861def3
rezaawrn <muhammadrezaaswarin.tkj2020@gmail.com>
2024-07-30 20:20:58 +0800

    Menambahkan Validasi Input Film yang Dipilih

--- tiketbioskop.cpp
+++ tiketbioskop.cpp
@@ -17,7 +17,7 @@ Film daftarFilm[MAX_FILM] = {
     {"Inside Out 2", "13:00", 100},
     {"Ipar adalah Maut ", "16:00", 100},
     {"Jurnal Risa", "19:00", 100},
-    {"Despicable Me 4", "21 : 00 ", 100}};
+    {"Despicable Me 4", "21:00", 100}};
 
 void tampilkanFilm()
 {
